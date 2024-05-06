874528aaadfec99fc85f3fc4fcc3bbb36cbe1f41
Soufyane Marsi <smarsi@e2r10p9.1337.ma>
2024-05-06 12:22:22 +0100

    save work

--- enemy/enemy.c
+++ enemy/enemy.c
@@ -6,7 +6,7 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:52:40 by smarsi            #+#    #+#             */
-/*   Updated: 2024/05/03 10:29:05 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/05/06 12:21:52 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void	right_route_helper(t_data *ptr, int *i, int *j)
 {
+	if (ptr->map_str[*i][*j + 1] == '0')
+	{
+		mlx_put_image_to_window(ptr->mlx, ptr->win, \
+		ptr->img.img, *j * 50, *i * 50);
+		ptr->map_str[*i][*j] = '0';
+		ptr->map_str[*i][*j + 1] = 'N';
+		(*j)++;
+	}
+	else
+		ptr->map_str[*i][*j] = 'n';
 	if (ptr->player.item_eaten > (ptr->num_item / 2))
 		ft_enemy_animation2(ptr, *i, *j, 'N');
 	else
@@ -25,11 +35,10 @@ void	right_route(t_data *ptr, int *i, int *j)
 	static int	c;
 	int			n;
 
-	c++;
-	n = (ptr->enemy.speed - ptr->player.item_eaten );
+	n = (ptr->enemy.speed - ptr->player.item_eaten);
 	if (n < 6)
 		n = 6;
-	if (c >= n)
+	if (c++ >= n)
 	{
 		if (ptr->map_str[*i][*j + 1] == 'P')
 		{
@@ -37,23 +46,22 @@ void	right_route(t_data *ptr, int *i, int *j)
 			ptr->enemy2.speed = 3;
 			ft_enemy_bombe(ptr, *i, *j);
 		}
-		else if (ptr->map_str[*i][*j + 1] == '0')
-		{
-			mlx_put_image_to_window(ptr->mlx, ptr->win, \
-			ptr->img.img, *j * 50, *i * 50);
-			ptr->map_str[*i][*j] = '0';
-			ptr->map_str[*i][*j + 1] = 'N';
-			(*j)++;
-		}
-		else
-			ptr->map_str[*i][*j] = 'n';
-		right_route_helper(ptr, i, j);
-		c = 0;
+		(right_route_helper(ptr, i, j), c = 0);
 	}
 }
 
 void	left_route_helper(t_data *ptr, int *i, int *j)
 {
+	if (ptr->map_str[*i][*j - 1] == '0')
+	{
+		mlx_put_image_to_window(ptr->mlx, ptr->win, \
+		ptr->img.img, *j * 50, *i * 50);
+		ptr->map_str[*i][*j] = '0';
+		ptr->map_str[*i][*j - 1] = 'n';
+		(*j)--;
+	}
+	else
+		ptr->map_str[*i][*j] = 'N';
 	if (ptr->player.item_eaten > (ptr->num_item / 2))
 		ft_enemy_animation2(ptr, *i, *j, 'n');
 	else
@@ -77,16 +85,6 @@ void	left_route(t_data *ptr, int *i, int *j)
 			ptr->enemy2.speed = 3;
 			ft_enemy_bombe(ptr, *i, *j);
 		}
-		else if (ptr->map_str[*i][*j - 1] == '0')
-		{
-			mlx_put_image_to_window(ptr->mlx, ptr->win, \
-			ptr->img.img, *j * 50, *i * 50);
-			ptr->map_str[*i][*j] = '0';
-			ptr->map_str[*i][*j - 1] = 'n';
-			(*j)--;
-		}
-		else
-			ptr->map_str[*i][*j] = 'N';
 		left_route_helper(ptr, i, j);
 		c = 0;
 	}
--- ft_draw.c
+++ ft_draw.c
@@ -6,53 +6,74 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:03:25 by smarsi            #+#    #+#             */
-/*   Updated: 2024/05/02 12:40:27 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/05/06 12:19:01 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	enemy_img(t_data *ptr)
+{
+	int (i), (j), (n);
+	n = 10;
+	if (ptr->num_item / 3 < ptr->player.item_eaten)
+		n = 6;
+	if (ptr->num_item / 2 < ptr->player.item_eaten)
+		n = 4;
+	i = 0;
+	if (ptr->enemy.speed == n)
+	{
+		while (ptr->map_str[i])
+		{
+			j = 0;
+			while (ptr->map_str[i][j])
+			{
+				if (ptr->map_str[i][j] == 'N')
+					right_route(ptr, &i, &j);
+				else if (ptr->map_str[i][j] == 'n')
+					left_route(ptr, &i, &j);
+				j++;
+			}
+			i++;
+		}
+		ptr->enemy.speed = 0;
+	}
+	ptr->enemy.speed++;
+}
+
 static void	ft_draw_helper(t_data *ptr, int i, int j)
 {
 	if (ptr->map_str[i][j] == 'P')
-	{
-		mlx_put_image_to_window(ptr->mlx, \
-		ptr->win, ptr->player.img, j * 50, i * 50);
-		ptr->player.pos_x = j;
-		ptr->player.pos_y = i;
-	}
+		ft_player_animation(ptr, i, j, 0);
+	else if (ptr->map_str[i][j] == '0')
+		display_img(ptr, NULL, i, j);
 	else if (ptr->map_str[i][j] == '1')
-		mlx_put_image_to_window(ptr->mlx, \
-		ptr->win, ptr->wall.img, j * 50, i * 50);
-	else if (ptr->map_str[i][j] == 'N' || ptr->map_str[i][j] == 'n')
-		mlx_put_image_to_window(ptr->mlx, ptr->win, \
-		ptr->enemy.img, j * 50, i * 50);
+		display_img(ptr, ptr->wall.img, i, j);
+	if (ptr->map_str[i][j] == 'N')
+		display_img(ptr, "./textures/player/enemy/right/0.xpm", i, j);
+	else if (ptr->map_str[i][j] == 'n')
+		display_img(ptr, "./textures/player/enemy/left/0.xpm", i, j);
 	else if (ptr->map_str[i][j] == 'E')
-		mlx_put_image_to_window(ptr->mlx, ptr->win, \
-		ptr->exit.img, j * 50, i * 50);
+		ft_exit(ptr, i, j);
 	else if (ptr->map_str[i][j] == 'C')
-		ptr->num_item++;
+		ft_collectible_animation(ptr, i, j);
 	else if (ptr->map_str[i][j] == 'B')
-		mlx_put_image_to_window(ptr->mlx, ptr->win, \
-		ptr->enemy2.img, j * 50, i * 50);
+		display_img(ptr, "./textures/player/enemy4/0.xpm", i, j);
 }
 
 int	ft_draw(t_data *ptr)
 {
-	int (i), (j);
 	i = 0;
 	while (ptr->map_str[i])
 	{
 		j = 0;
 		while (ptr->map_str[i][j])
 		{
-			mlx_put_image_to_window(ptr->mlx, ptr->win, \
-			ptr->img.img, j * 50, i * 50);
 			ft_draw_helper(ptr, i, j);
 			j++;
 		}
 		i++;
 	}
-	mlx_string_put(ptr->mlx, ptr->win, 25, 15, 0x00FF0000, "MOVES : 0");
+	print_moves2(ptr);
 	return (0);
 }
